46ebae866b74ca3d935b15d6b2c4208f12e10eed
LeoJimenezG <leonardojimenezgz@gmail.com>
2025-04-20 15:41:46 -0600

    base initial code

---
+++ algorithms.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+class SortingAlgorithms
+{
+    public:
+        SortingAlgorithms()
+        {
+
+        }
+
+        int* bogo_sort(int list[])
+        {
+
+        }
+
+        int* selection_sort(int list[])
+        {
+
+        }
+
+        int* bubble_sort(int list[])
+        {
+
+        }
+
+        int* merge_sort(int list[])
+        {
+
+        }
+
+        bool check_sort(int list[])
+        {
+
+        }
+};
