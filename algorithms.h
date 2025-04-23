1671be5ad5a93ec571566aa9061937fdff398870
LeoJimenezG <leonardojimenezgz@gmail.com>
2025-04-22 19:33:07 -0600

    create base header file declaring SortingAlgorithms class

---
+++ algorithms.h
@@ -0,0 +1,26 @@
+#ifndef ALGORITHMS_H 
+#define ALGORITHMS_H
+
+#include <vector>
+
+class SortingAlgorithms
+{
+    public:
+        SortingAlgorithms ();
+
+        void test ();
+
+        std::vector<int> bogo_sort (const std::vector<int> values);
+
+        std::vector<int> selection_sort (const std::vector<int> values);
+
+        std::vector<int> bubble_sort (const std::vector<int> values);
+
+        std::vector<int> merge_sort (const std::vector<int> values);
+
+        std::vector<int> quick_sort (const std::vector<int> values);
+
+        bool check_sort (const std::vector<int> values);
+};
+
+#endif
