e5d8c5cd2e23e40d23bdeac70df6927208a294ee
LeoJimenezG <leonardojimenezgz@gmail.com>
2025-04-23 19:21:38 -0600

    test selection and bubble sort

--- main.cpp
+++ main.cpp
@@ -5,13 +5,30 @@ SortingAlgorithms sorting_algorithms;
 
 int main ()
 {
-    std::vector numbers = {4, 0, 1, 5, 2, 3};
+    std::vector numbers = {4, 0, 1, 5, 2, 3, 8, 9, 7, 6};
+    std::cout << "Original vector: ";
     sorting_algorithms.print_vector(numbers);
 
     std::cout << '\n';
 
-    std::vector sorted_numbers = sorting_algorithms.bogo_sort(numbers);
-    sorting_algorithms.print_vector(sorted_numbers);
+    std::vector bogo_sorted_numbers = sorting_algorithms.bogo_sort(numbers);
+    std::cout << "Bogo Sorted vector: ";
+    sorting_algorithms.print_vector(bogo_sorted_numbers);
+
+    std::cout << '\n';
+    
+    std::vector selection_sorted_numbers = sorting_algorithms.selection_sort(numbers);
+    std::cout << "Selection Sorted vector: ";
+    sorting_algorithms.print_vector(selection_sorted_numbers);
+
+    std::cout << '\n';
+
+    std::vector bubble_sorted_numbers = sorting_algorithms.bubble_sort(numbers);
+    std::cout << "Bubble Sorted vector: ";
+    sorting_algorithms.print_vector(bubble_sorted_numbers);
+
+    std::cout << '\n';
+
 
     return 0;
 }
